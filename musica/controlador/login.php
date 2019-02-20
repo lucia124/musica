0297a3e9b08030cde307cb0fbb6113db12a486c4
lucia124 <47173746+lucia124@users.noreply.github.com>
2019-02-20 09:30:37 +0100

    Update login.php

--- musica/controlador/login.php
+++ musica/controlador/login.php
@@ -1,7 +1,7 @@
 
 
 <?php
-require('../modelo/iniciarSesion.php')
+require('../modelo/iniciarSesion.php');
 $email=$_REQUEST['email'];
 $contrasena=$_REQUEST['apellido'];
 darAltaUsuario($email,$contrasena);
